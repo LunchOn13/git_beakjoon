09c37710bd2629fad1530d1e304b3edae7ddc491
unknown <>
2016-09-12 07:36:02 +0900

    beakjoon 2016/09/12 - 3

---
+++ 1037.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int number;
+//	cin >> number;
+//	vector<int> a(number);
+//
+//	for (int i = 0; i < number; i++)
+//	{
+//		cin >> a[i];
+//	}
+//
+//	int max = a[0];
+//	int min = a[0];
+//	for (int i = 0; i < number; i++)
+//	{
+//		if (max < a[i])
+//		{
+//			max = a[i];
+//		}
+//	}
+//
+//	for (int i = 0; i < number; i++)
+//	{
+//		if (min > a[i])
+//		{
+//			min = a[i];
+//		}
+//	}
+//
+//	cout << max*min;
+//
+//	return 0;
+//}
