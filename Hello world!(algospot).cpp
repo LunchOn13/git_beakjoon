43a8f8a7d21ba38cc72dc9ec52670552bcfa07e8
DESKTOP-7H4ICI8\이종원 <jay09144@gmail.com>
2016-09-17 10:19:45 +0900

    beakjoon 2016/09/17 - 8,9

---
+++ Hello world!(algospot).cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//
+//int main()
+//{
+//
+//	int testcase;
+//	cin >> testcase;
+//	string arr[50] ;
+//
+//	for (int i = 0; i < testcase; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//	
+//	for (int i = 0; i < testcase; i++)
+//	{
+//		cout << "Hello, " << arr[i] << "!\n";
+//	}
+//	return 0;
+//}
+//
+////char ·Î ¾µ¶§
+///*
+//int main()
+//{
+//	int num;
+//	cin >> num;
+//	char arr[50];
+//	for (int i = 0; i < num; i++) {
+//		cin >> arr;
+//		cout << "Hello, " << arr << "!\n";
+//	}
+//	return 0;
+//}*/
