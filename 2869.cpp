b07e02605ddbda1b62dd8b67f1334c3677e8d0be
DESKTOP-7H4ICI8\이종원 <jay09144@gmail.com>
2016-10-05 18:09:30 +0900

    beakjoon 2016/10/05 - 25

---
+++ 2869.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, v;
+//
+//	cin >> a >> b >> v;
+//
+//	int counter = 0;
+//	int height = 0;
+//	while(1)
+//	{
+//		++counter;
+//		height += a;
+//		if (height >= v)
+//		{
+//			cout << counter;
+//			return 0;
+//		}
+//		height -= b;
+//	}
+//}
