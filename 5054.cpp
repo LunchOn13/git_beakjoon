aa4fa455154ddd0acd75315caaf26c7141d42bd0
DESKTOP-7H4ICI8\이종원 <jay09144@gmail.com>
2016-10-10 22:05:39 +0900

    beakjoon 2016/10/10 - 28

---
+++ 5054.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int testcase;
+//	cin >> testcase;
+//
+//	for (int i = 0; i < testcase; i++)
+//	{
+//		int shop;
+//		cin >> shop;
+//		int min = 1000;
+//		int max = -1000;
+//		for (int j = 0; j < shop; j++)
+//		{
+//			
+//			int number;
+//			cin >> number;
+//			if (number < min)
+//			{
+//				min = number;
+//			}
+//			if (number > max)
+//			{
+//				max = number;
+//			}
+//		}
+//		cout << 2*(max - min) << endl;
+//	}
+//}
