d7562e0333dcb67ccf19daf1c6507735d938d2dc
DESKTOP-7H4ICI8\이종원 <jay09144@gmail.com>
2016-10-25 15:01:40 +0900

    beakjoon 2016/10/25 - 38

---
+++ 1193(분수찾기).cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	unsigned int input;
+//		cin >> input;
+//		unsigned int n = 1;
+//		unsigned int k = 1;
+//		while (true)
+//		{
+//			if (n >= input)
+//			{
+//				break;
+//			}
+//			k++;
+//			n += k;
+//		}
+//		if(k%2==1)
+//		{
+//			cout << 1 + (n - input) << "/" << k - (n - input) << endl;
+//		}
+//		else
+//		{ 
+//			cout << k - (n - input) << "/" << 1 + (n - input) << endl;
+//		}
+//			return 0;
+//	}
+//	
