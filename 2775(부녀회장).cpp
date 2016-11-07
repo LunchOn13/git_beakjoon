454748a6ac621c91f5c6d7982b774641f2df43a8
DESKTOP-7H4ICI8\이종원 <jay09144@gmail.com>
2016-11-07 13:19:45 +0900

    solving

---
+++ 2775(부녀회장).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int testcase;
+	cin >> testcase;
+
+	int n, k;
+
+
+
+	for (int i = 0; i < testcase; i++)
+	{
+		cin >> k;
+		cin >> n;
+
+
+
+
+	}
+
+
+
+
+	return 0;
+}
